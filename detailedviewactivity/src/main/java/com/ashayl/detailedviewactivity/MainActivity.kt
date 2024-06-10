76aff662747d413c7877612796c540706cd4afd3
Ashayl <ST10443318@vcconnect.edu.za>
2024-06-10 13:27:02 +0200

    push4

--- detailedviewactivity/src/main/java/com/ashayl/detailedviewactivity/MainActivity.kt
+++ detailedviewactivity/src/main/java/com/ashayl/detailedviewactivity/MainActivity.kt
@@ -7,5 +7,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonMS: Button = findViewById<Button>(R.id.buttonMS)
+        val tvDetails = findViewById<TextView>(R.id.tvDetails)
+        val displayData = intent.getStringExtra("DISPLAY_DATA")
+        tvDetails.text = displayData
+
+
     }
 }
